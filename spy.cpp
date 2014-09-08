b73719dba581f2855b7009ca79b4154598f74563
Andrew <truffle>
2014-09-08 10:19:05 -0400

    Print signals by name rather than description.

--- spy.cpp
+++ spy.cpp
@@ -1317,6 +1317,46 @@ static std::string elide_string(const std::string &str, int size)
 	return elided;
 }
 
+static const char *signalname(int signal)
+{
+	switch (signal)
+	{
+		case SIGHUP:	return "SIGHUP";
+		case SIGINT:	return "SIGINT";
+		case SIGQUIT:	return "SIGQUIT";
+		case SIGILL:	return "SIGILL";
+		case SIGTRAP:	return "SIGTRAP";
+		case SIGABRT:	return "SIGABRT";
+		case SIGBUS:	return "SIGBUS";
+		case SIGFPE:	return "SIGFPE";
+		case SIGKILL:	return "SIGKILL";
+		case SIGUSR1:	return "SIGUSR1";
+		case SIGSEGV:	return "SIGSEGV";
+		case SIGUSR2:	return "SIGUSR2";
+		case SIGPIPE:	return "SIGPIPE";
+		case SIGALRM:	return "SIGALRM";
+		case SIGTERM:	return "SIGTERM";
+		case SIGSTKFLT:	return "SIGSTKFLT";
+		case SIGCHLD:	return "SIGCHLD";
+		case SIGCONT:	return "SIGCONT";
+		case SIGSTOP:	return "SIGSTOP";
+		case SIGTSTP:	return "SIGTSTP";
+		case SIGTTIN:	return "SIGTTIN";
+		case SIGTTOU:	return "SIGTTOU";
+		case SIGURG:	return "SIGURG";
+		case SIGXCPU:	return "SIGXCPU";
+		case SIGXFSZ:	return "SIGXFSZ";
+		case SIGVTALRM:	return "SIGVTALRM";
+		case SIGPROF:	return "SIGPROF";
+		case SIGWINCH:	return "SIGWINCH";
+		case SIGIO:		return "SIGIO";
+		case SIGPWR:	return "SIGPWR";
+		case SIGSYS:	return "SIGSYS";
+		case SIGSTKSZ:	return "SIGSTKSZ";
+		default:		return strsignal(signal);
+	}
+}
+
 template <PROMPT_TYPE prompt>
 static void execute_command(const char *command)
 {
@@ -1442,7 +1482,7 @@ static void execute_command(const char *command)
 		else
 		{
 			char buf[BUFSIZE];
-			snprintf(buf, BUFSIZE, "Terminated by '%s'", strsignal(WTERMSIG(status)));
+			snprintf(buf, BUFSIZE, "Terminated by %s", signalname(WTERMSIG(status)));
 			status_string = buf;
 		}
 	}
